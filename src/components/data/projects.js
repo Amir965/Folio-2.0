f3c6b1c1a7c6c8a3d6a86f938bafb6fbd22b96c0
Amir965 <sohailaamir965@gmail.com>
2023-06-22 18:01:52 +0530

    logo update

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -4,7 +4,7 @@ export const ProjectData = [
     title: "Consumer-Side Portal",
     about:
       "This is the consumer-side application for drug purposes. Made up of using the Javascript and NextJs. This application is basically for the users. Users first log in to this site and order their medicines through this site and get their medicines at home or at the store.",
-    tags: ["Javascript", "NextJs"],
+    tags: ["Javascript", "ReactJs","Bootstrap"],
     demo: "https://consapp.irevo.in/",
     github: "www.github.com",
     image: require("../../assets/icons/project1.png"),
@@ -15,7 +15,7 @@ export const ProjectData = [
     title: "Tarena iRevo Portal ",
     about:
       "This is the Irevo portal for Retailers. Made up of using the Javascript, Reactjs and Redux.This site illustrates the summary of retailers like the delivery options(store, home), payment options(cash, UPI), montly sales options, and also the most sales drugs.",
-    tags: ["Javascript", "ReactJs", "Redux"],
+    tags: ["Javascript", "ReactJs", "Redux",],
     demo: "http://164.52.193.216:8082/",
     github: "www.github.com",
     image: require("../../assets/icons/project3.png"),
@@ -29,5 +29,15 @@ export const ProjectData = [
     demo: "https://expenser-app.netlify.app/",
     github: "https://github.com/Amir965/Expenser_App ",
     image: require("../../assets/icons/project4.png"),
+  },
+  {
+    id: 4,
+    title: "Law For Me ",
+    about:
+      "This is U.S based project.This website for client who want to need for Lawyers Suppose we have any case or any FIR something so in that we have not much of Knowledge to find for a good Lawyers for the particular subject,so that Law For Me offers expert legal advice, document creation, and dispute resolution without the need for a lawyer. This saves time and money while simplifying complex areas of the law. Our platform also provides access to city lawyers with live video consultations and chat. We offer a virtual dispute resolution platform using mediators or arbitrators to resolve disputes, avoiding the stress and expense of Court. Our innovative approach provides new thinking in the legal industry.",
+    tags: ["Javascript", "ReactJs", "Bootstrap"],
+    demo: "https://lawforme.co.uk/",
+    github: "https://github.com/Amir965 ",
+    image: require("../../assets/icons/lfm.png"),
   },
 ];
--- src/components/data/work.js
+++ src/components/data/work.js
@@ -1,13 +1,21 @@
 export const WorkData = [
   {
     company: "Company ",
-    name:"Tarnea Technology Solution Ltd.",
-    designation: "Ui Trainee",
+    name:"Tarnea Technology Solution Pvt Ltd.",
+    designation: "Front End Developer",
     dateJoining: "01-09-2021",
-    dateEnd: "Present",
-    companyLogo:
-        "https://media-exp1.licdn.com/dms/image/C510BAQHTZQuAhAUWxg/company-logo_100_100/0/1519870942664?e=1663200000&v=beta&t=sTI_u_itKxRODHf6td4Y_ViTwOl5gpMJXFn5PnmwGKw",
+    dateEnd: "01-11-2022",
+    companyLogo:require("../../assets/icons/Tarnea-logo.jpg"),
     work: "I am working at Tarnea Technology Solution Ltd, closely with the Tech-team on a Consumer-side application and Tarnea iRrevo portal side as a Front-End Developer.",
+  },
+  {
+    company: "Company ",
+    name:"42Works Info Solutions Pvt Ltd.",
+    designation: "Web Developer",
+    dateJoining: "13-03-2023",
+    dateEnd: "Present",
+    companyLogo:require("../../assets/icons/42works.png"),
+    work: "Currently I'm working at 42Works info Solutions Pvt Ltd as a web developer  closely with the backend Team on a Law For Me Project.",
   },
   
 ];
