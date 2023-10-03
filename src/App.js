d135546f18974227e963a7997c7f676f1a903ba7
Amir965 <sohailaamir965@gmail.com>
2023-10-03 20:53:09 +0530

    loader issue fixed

--- src/App.js
+++ src/App.js
@@ -2,16 +2,15 @@ import React, { useState, useEffect } from "react";
 import Home from "./components/home/index.js";
 import Loader from "./components/loader/index.js";
 function App() {
-  // const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   setTimeout(() => {
-  //     setLoading(false);
-  //   }, 2500);
-  // }, []);
-  // return <>{loading ? <Loader /> : <Home />}</>;
-  return <><Home /></>;
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
+  return <>{loading ? <Loader /> : <Home />}</>;
 }
 
 export default App;
